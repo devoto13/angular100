722f23f96c7c61c5a71b13e54a8b8947cdcf945c
Yaroslav Admin <devoto13@gmail.com>
2021-06-04 18:35:42 +0200

    Fix fa-icon not rendering

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TollbarComponent } from 'src/app/components/tollbar/tollbar.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [TollbarComponent],
-  imports: [
-    CommonModule,
-  ]
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [TollbarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
